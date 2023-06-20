bd0d6ae93ae7205534dd7d1fbcf71f82a7652143
shiv gaurav <shivgaurav27@gmail.com>
2023-06-20 19:56:44 +0530

    usememo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import ParentComponent from './ParentComponent';
+import ParentComponent from './useCallbackHooks/ParentComponent';
+import Memo from './useMemohooks/Memo';
 
 // link for reading : https://dev.to/ahmedgmurtaza/react-performance-optimization-usememo-vs-usecallback-2p2a#:~:text=To%20summarize%2C%20the%20main%20difference,useMemo%20returns%20a%20memoized%20value.
 
 function App() {
 	return (
 		<div className='App'>
-			
-			<ParentComponent />
+			{/* <ParentComponent /> */}
+			<Memo />
 		</div>
 	);
 }
