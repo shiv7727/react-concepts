f5c00d1f7b39e9d27fc04941abb257201469a985
shiv gaurav <shivgaurav27@gmail.com>
2023-06-20 13:34:17 +0530

    Higher order functions

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// HO functions are the functions which takes another function as a paramter
+	// multiplyBy is a higher order function
+	const multiplyBy = (num1) => {
+		return (num2) => {
+			return num1 * num2;
+		};
+	};
+
+	const multiplyByTwo = multiplyBy(2);
+	const multiplyByFive = multiplyBy(5);
+
+	console.log(multiplyByTwo(5));
+	console.log(multiplyByFive(10));
+
+	return (
+		<div className='App'>
+			<h1>Higher order Function </h1>
+		</div>
+	);
 }
 
 export default App;
