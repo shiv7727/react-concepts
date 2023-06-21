b80d66c0dac5c971a29f3b00a772a451797cc2af
shiv gaurav <shivgaurav27@gmail.com>
2023-06-21 07:57:06 +0530

    axios api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+let url = 'https://jsonplaceholder.typicode.com/users';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState();
+	useEffect(() => {
+		const res = axios.get(url).then((resp) => {
+			setData(resp.data);
+		});
+	}, []);
+
+	return (
+		<div className='App'>
+			<ul>
+				{data?.map((item) => (
+					<li key={item.id}>{item.name}</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default App;
