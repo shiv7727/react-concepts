004aff8b729da0b0b6deddb1c90c51c8459de4ad
shiv gaurav <shivgaurav27@gmail.com>
2023-07-13 20:40:27 +0530

    useMemo best example to learn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React, { useState, useMemo } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	const [countInTens, setCountInTens] = useState(0);
+
+	const multyplyCountByFive = useMemo(() => {
+		console.log('calling memo function ');
+		return count * 5;
+	}, [count]);
+
+	return (
+		<div className='App'>
+			<p> {count}</p>
+			<button onClick={() => setCount((prevCount) => prevCount + 1)}>
+				countIncrement by 1
+			</button>
+			<p> {countInTens}</p>
+			<button onClick={() => setCountInTens((prevCount) => prevCount + 10)}>
+				countIncrement by 1
+			</button>
+			<hr />
+			<p>{multyplyCountByFive}</p>
+		</div>
+	);
 }
 
 export default App;
