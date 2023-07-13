b0cc3ae337e890b795e7166090d53d038067da86
shiv gaurav <shivgaurav27@gmail.com>
2023-07-13 21:37:01 +0530

    Stopwatch verson 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { useRef } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [time, setTime] = useState(0);
+
+	useEffect(() => {
+		// handleTime();
+		return () => clearInterval(id.current);
+	}, []);
+
+	let id = useRef();
+
+	function handleTime() {
+		id.current = setInterval(() => {
+			setTime((prevState) => prevState + 1);
+			console.log('time', time);
+		}, 1000);
+	}
+
+	return (
+		<div className='App'>
+			<h1>Stopwatch</h1>
+			<h1>{time}</h1>
+			<button onClick={handleTime}>start</button>
+			<button onClick={() => clearInterval(id.current)}>pause</button>
+			<button
+				onClick={() => {
+					clearInterval(id.current);
+					setTime(0);
+				}}
+			>
+				Reset
+			</button>
+		</div>
+	);
 }
 
 export default App;
