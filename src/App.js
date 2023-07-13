c8e51012af71f60f2fe30af107d7bcd431d89278
shiv gaurav <shivgaurav27@gmail.com>
2023-07-13 21:56:07 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import { useRef } from 'react';
 
 function App() {
-	const [time, setTime] = useState(0);
+	const [time, setTime] = useState({
+		hr: 0,
+		min: 0,
+		sec: 0,
+	});
 
 	useEffect(() => {
 		// handleTime();
@@ -14,7 +18,15 @@ function App() {
 
 	function handleTime() {
 		id.current = setInterval(() => {
-			setTime((prevState) => prevState + 1);
+			setTime((prevState) => {
+				if (prevState.sec == 60) {
+					return { ...prevState, min: prevState.min + 1, sec: 0 };
+				}
+				if (prevState.min == 60) {
+					return { ...prevState, hr: prevState.hr + 1, min: 0, sec: 0 };
+				}
+				return { ...prevState, sec: prevState.sec + 1 };
+			});
 			console.log('time', time);
 		}, 1000);
 	}
@@ -22,13 +34,32 @@ function App() {
 	return (
 		<div className='App'>
 			<h1>Stopwatch</h1>
-			<h1>{time}</h1>
+			<h1>
+				{time.hr?.toLocaleString('en-US', {
+					minimumIntegerDigits: 2,
+					useGrouping: false,
+				})}
+				:
+				{time.min?.toLocaleString('en-US', {
+					minimumIntegerDigits: 2,
+					useGrouping: false,
+				})}
+				:
+				{time.sec?.toLocaleString('en-US', {
+					minimumIntegerDigits: 2,
+					useGrouping: false,
+				})}
+			</h1>
 			<button onClick={handleTime}>start</button>
 			<button onClick={() => clearInterval(id.current)}>pause</button>
 			<button
 				onClick={() => {
 					clearInterval(id.current);
-					setTime(0);
+					setTime({
+						hr: 0,
+						min: 0,
+						sec: 0,
+					});
 				}}
 			>
 				Reset
