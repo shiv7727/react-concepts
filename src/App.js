3a0e67bc0f18ac6e09b047962e9302d96e2879b3
shiv gaurav <shivgaurav27@gmail.com>
2023-07-23 10:08:29 +0530

    ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const inputRef = useRef(null);
+
+	useEffect(() => {
+		inputRef.current.focus();
+	}, []);
+
+	return (
+		<div className='App'>
+			<input type='text' name='name' id='name' ref={inputRef} />
+		</div>
+	);
 }
 
 export default App;
