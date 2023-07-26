c77795d9bfd6354599e1b4a2d9012b76ab019258
shiv gaurav <shivgaurav27@gmail.com>
2023-07-26 11:39:15 +0530

    stopwatch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './App.css';
 
 function App() {
 	const [time, setTime] = useState(0);
 	const id = useRef(null);
 
+	useEffect(() => {
+		// handleTime();
+		return () => clearInterval(id.current);
+	}, []);
+
 	const handleTime = () => {
-		setInterval(() => {
+		id.current = setInterval(() => {
 			setTime((prevState) => prevState + 1);
 		}, 1000);
 	};
@@ -14,6 +19,16 @@ function App() {
 	return (
 		<div className='App'>
 			<h3>{time}</h3>
+			<button onClick={handleTime}>start</button>
+			<button onClick={() => clearInterval(id.current)}>pause</button>
+			<button
+				onClick={() => {
+					clearInterval(id.current);
+					setTime(0);
+				}}
+			>
+				Reset
+			</button>
 		</div>
 	);
 }
