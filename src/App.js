f273fb0fca7a80f94e6059a86f8ee013df5094ad
shiv gaurav <shivgaurav27@gmail.com>
2023-07-26 15:47:39 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const arr = [1, 2, [3, 4, 5, [6, 7, 8, [9, 0]]]];
+	const newArr = [];
+
+	function flatArryFunc(arr) {
+		arr.forEach((item) => {
+			if (typeof item === 'number') {
+				newArr.push(item);
+			} else if (Array.isArray(item)) {
+				flatArryFunc(item);
+			}
+			// console.log(item);
+		});
+	}
+	flatArryFunc(arr);
+	console.log('logic function ', newArr);
+
+	console.log('flat method', arr.flat(Infinity));
+
+	return (
+		<div className='App'>
+			<h1>JS</h1>
+		</div>
+	);
 }
 
 export default App;
