ca7d9efe4d4c89f16513faf42ac2d3ccb8640129
shiv gaurav <shivgaurav27@gmail.com>
2023-08-10 17:55:49 +0530

    typescript form

--- src/Form/RegistrationForm.tsx
+++ src/Form/RegistrationForm.tsx
@@ -1,25 +1,80 @@
 import React, { useReducer } from 'react'
 import "./RegistrationForm.css"
 
+const UPDATE_FORM = 'UPDATE_FORM';
+
 const initialDetails = {
-    email: { value: '', hasError: false, error: '', touched: false }
-}
+    email: { value: '', touched: false, hasError: false, error: '' },
+    isFormValid: false,
+};
 
-const formReducer = () =>{
-    
-}
+const formReducer = (state: any, action: any) => {
+    if (action.type === UPDATE_FORM) {
+        const { name, value, hasError, error, touched, isFormValid } = action.data;
+        return {
+            ...state,
+            [name]: { ...state[name], value, hasError, error, touched },
+            isFormValid,
+        };
+    } else {
+        return state;
+    }
+};
+
+const validateInput = (name: any, value: any) => {
+    const regExp = /^([\w\.\+]{1,})([^\W])(@)([\w]{1,})(\.[\w]{1,})+$/;
+
+    let hasError: boolean = false;
+    let error: string = '';
+
+    switch (name) {
+        case 'email':
+            if (!regExp.test(value)) {
+                hasError = true;
+                error = 'Invalid email address !';
+            }
+            break;
+        default:
+            break;
+    }
+    return { hasError, error };
+};
+
+const onInputChange = (name: any, value: any, dispatch: any, regDetails: any) => {
+    const { hasError, error } = validateInput(name, value);
+    dispatch({
+        type: UPDATE_FORM,
+        data: { name, value, hasError, error, touched: false },
+    });
+};
+
+const onFocusOut = (name: string, value: any, dispatch: React.Dispatch<any>, regDetails: { [p: string]: any }) => {
+    const { hasError, error } = validateInput(name, value);
+    let isFormValid = true;
+    for (const key in regDetails) {
+        const item = regDetails[key];
+
+        if ((key === name && hasError) || (key !== name && item?.hasError)) {
+            isFormValid = false;
+            break;
+        }
+    }
+    dispatch({
+        type: UPDATE_FORM,
+        data: { name, value, hasError, error, touched: true, isFormValid },
+    });
+};
+
+const handleBlur = (e: any, dispatch: any, regDetails: any) => {
+    onFocusOut(e.target.name, e.target.value, dispatch, regDetails);
+};
 
 
 
-interface props {
 
-}
-const RegistrationForm: React.FC<props> = () => {
+const RegistrationForm: React.FC = () => {
 
     const [regDetails, dispatch] = useReducer(formReducer, initialDetails)
-
-
-
 
 
 
@@ -37,7 +92,10 @@ const RegistrationForm: React.FC<props> = () => {
                     <hr />
 
                     <label htmlFor="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" id="email" />
+                    <input type="text" placeholder="Enter Email" name="email" id="email"
+                        onChange={(e) => onInputChange(e.target.name, e.target.value, dispatch, regDetails)}
+                        onBlur={(e) => handleBlur(e, dispatch, regDetails)}
+                    />
 
                     <label htmlFor="firstName"><b>First Name</b></label>
                     <input type="text" placeholder="Enter First Name" name="firstName" id="firstName" />
