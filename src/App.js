20d68e2c47a027ca7fcee6510b5ab4b14dd6d5a7
Shiv Gaurav <Shivgaurav27@gmail.com>
2023-10-06 13:42:42 +0530

    accordion part 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const faqs = [
+	{
+		title: 'Where are these chairs assembled?',
+		text: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.',
+	},
+	{
+		title: 'How long do I have to return my chair?',
+		text: 'Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.',
+	},
+	{
+		title: 'Do you ship to countries outside the EU?',
+		text: 'Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!',
+	},
+];
 
 function App() {
+	return <Accordion data={faqs} />;
+}
+
+export default App;
+
+function Accordion(props) {
+	const { data } = props;
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit123 <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
+		<div className='accordion'>
+			{data.map((el, i) => (
+				<AccordionItem num={i} title={el.title} text={el.text} />
+			))}
 		</div>
 	);
 }
 
-export default App;
+function AccordionItem(props) {
+	const { title, text, num } = props;
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleToggle = () => {
+		setIsOpen((isOpen) => !isOpen);
+	};
+
+	return (
+		<div className='item' onClick={handleToggle}>
+			<p className='number'>{num < 9 ? `0${num + 1}` : num}</p>
+			<p className='title'>{title}</p>
+			<p className='icon'>{isOpen ? '-' : '+'}</p>
+			{isOpen && <div className='content-box'>{text}</div>}
+		</div>
+	);
+}
