c4309fb47ad5c4f7dccc4d8e5fc540252e0cb86f
Shiv Gaurav <Shivgaurav27@gmail.com>
2023-10-11 15:16:00 +0530

    propTypes added

--- src/StartRating.jsx
+++ src/StartRating.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
 
 const containerStyle = {
 	display: 'flex',
@@ -8,6 +9,16 @@ const containerStyle = {
 
 const startContainerStyle = {
 	display: 'flex',
+};
+
+StarRating.propTypes = {
+	maxRating: PropTypes.number.isRequired,
+	defaultRating: PropTypes.number,
+	color: PropTypes.string,
+	size: PropTypes.number,
+	messages: PropTypes.array,
+	className: PropTypes.string,
+	onSetRating: PropTypes.func,
 };
 
 function StarRating({
