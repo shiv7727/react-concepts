536aba6cbf2523dcf2751b7782be1637bfd70232
shiv gaurav <shivgaurav27@gmail.com>
2024-05-22 08:31:59 +0530

    new test

--- src/App.js
+++ src/App.js
@@ -1,72 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import './App.css';
-import { useRef } from 'react';
 
-function App() {
-	
-	const [time, setTime] = useState({
+const App = () => {
+	const initialState = {
 		hr: 0,
 		min: 0,
 		sec: 0,
-	});
-
-	useEffect(() => {
-		// handleTime();
-		return () => clearInterval(id.current);
-	}, []);
-
+	};
+	const [time, setTime] = useState(initialState);
 	let id = useRef();
 
-	function handleTime() {
+	const handleTime = () => {
 		id.current = setInterval(() => {
+			console.log('calling setTime');
 			setTime((prevState) => {
-				if (prevState.sec == 60) {
-					return { ...prevState, min: prevState.min + 1, sec: 0 };
+				if (prevState.sec === 60) {
+					return { ...prevState, hr: 0, min: prevState.min + 1, sec: 0 };
 				}
-				if (prevState.min == 60) {
+				if (prevState.min === 60) {
 					return { ...prevState, hr: prevState.hr + 1, min: 0, sec: 0 };
 				}
 				return { ...prevState, sec: prevState.sec + 1 };
 			});
-			console.log('time', time);
 		}, 1000);
-	}
+	};
 
 	return (
 		<div className='App'>
-			<h1>Stopwatch</h1>
+			<h1>StopWatch</h1>
 			<h1>
-				{time.hr?.toLocaleString('en-US', {
-					minimumIntegerDigits: 2,
-					useGrouping: false,
-				})}
-				:
-				{time.min?.toLocaleString('en-US', {
-					minimumIntegerDigits: 2,
-					useGrouping: false,
-				})}
-				:
-				{time.sec?.toLocaleString('en-US', {
-					minimumIntegerDigits: 2,
-					useGrouping: false,
-				})}
+				<p>
+					{time.hr.toLocaleString('en-US', {
+						minimumIntegerDigits: 2,
+						useGrouping: false,
+					})}
+					:
+					{time.min.toLocaleString('en-US', {
+						minimumIntegerDigits: 2,
+						useGrouping: false,
+					})}
+					:
+					{time.sec.toLocaleString('en-US', {
+						minimumIntegerDigits: 2,
+						useGrouping: false,
+					})}
+				</p>
 			</h1>
-			<button onClick={handleTime}>start</button>
+			<button onClick={handleTime}>Start</button>
 			<button onClick={() => clearInterval(id.current)}>pause</button>
 			<button
 				onClick={() => {
 					clearInterval(id.current);
-					setTime({
-						hr: 0,
-						min: 0,
-						sec: 0,
-					});
+					setTime(0);
 				}}
 			>
 				Reset
 			</button>
 		</div>
 	);
-}
+};
 
 export default App;
